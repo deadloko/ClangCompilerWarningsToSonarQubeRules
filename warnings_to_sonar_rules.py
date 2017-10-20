7166feaaccf64e7c0fa34f71c0455eba0d492986
Andrew Newman <deadloko@gmail.com>
2017-10-20 19:26:50 +0300

    Reformat code

--- warnings_to_sonar_rules.py
+++ warnings_to_sonar_rules.py
@@ -2,9 +2,10 @@
 # coding: utf-8
 from bs4 import BeautifulSoup
 import requests
-import os
 
-def removeNonAscii(s): return "".join(i for i in s if ord(i)<128)
+
+def remove_non_ascii(s): return "".join(i for i in s if ord(i) < 128)
+
 
 def get_rules_clang_doc():
     warning_list_slices = []
@@ -12,10 +13,11 @@ def get_rules_clang_doc():
     diagnostic_page = requests.get(clang_diagnostic_url)
     diagnostic_content = diagnostic_page.content
     soup = BeautifulSoup(diagnostic_content, 'lxml')
-    warnings_list = soup.find('div', {'class':'section', 'id':'diagnostic-flags'}).findAll('h3')
-    for warning in warnings_list:
-        warning_list_slices.append(removeNonAscii(warning.text))
+    warnings_list = soup.find('div', {'class': 'section', 'id': 'diagnostic-flags'}).findAll('h3')
+    for soup_warning in warnings_list:
+        warning_list_slices.append(remove_non_ascii(soup_warning.text))
     return warning_list_slices
+
 
 def get_rules_nonofficial_doc():
     warning_list_slices = []
@@ -23,25 +25,27 @@ def get_rules_nonofficial_doc():
     warnings_page = requests.get(clang_warnings_url)
     warnings_content = warnings_page.content
     soup = BeautifulSoup(warnings_content, 'lxml')
-    warning_list = soup.find('div', {'id':'main'}).findAll('td', {'itemprop':'name'})
-    for warning in warning_list:
-        warning_list_slices.append(warning.text)
+    warning_list = soup.find('div', {'id': 'main'}).findAll('td', {'itemprop': 'name'})
+    for soup_warning in warning_list:
+        warning_list_slices.append(soup_warning.text)
     return warning_list_slices
+
 
 if __name__ == '__main__':
     warnings_official = get_rules_clang_doc()
 
-    file = open('sonar_rules', 'w')
+    rules_file = open('sonar_rules', 'w')
 
-    file.writelines('<rules>\n')
+    rules_file.writelines('<rules>\n')
 
     for warning in warnings_official:
-        file.write('  <rule>\n')
-        file.write('    <key>{}-clang-compiler</key>\n'.format(warning))
-        file.write('    <name>{}</name>\n'.format(warning))
-        file.write('    <description>Clang compiler warning, message will contain additional description</description>\n')
-        file.write('    <severity>Minor</severity>\n')
-        file.write('    <type>BUG</type>\n')
-        file.write('  </rule>\n')
+        rules_file.write('  <rule>\n')
+        rules_file.write('    <key>{}-clang-compiler</key>\n'.format(warning))
+        rules_file.write('    <name>{}</name>\n'.format(warning))
+        rules_file.write(
+            '    <description>Clang compiler warning, message will contain additional description</description>\n')
+        rules_file.write('    <severity>Minor</severity>\n')
+        rules_file.write('    <type>BUG</type>\n')
+        rules_file.write('  </rule>\n')
 
-    file.write('</rules>\n')
+    rules_file.write('</rules>\n')
