b7f8929696f98319587a8c7c4c92cb590687dd4a
Andrew Newman <deadloko@gmail.com>
2017-10-21 11:10:16 +0300

    Add clang_compiler report converter

---
+++ report_converter.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+
+from xml.sax.saxutils import escape
+import re
+
+def ConvertDiagnosticLineToSonqarqube(item):
+    id, line, message, source_file = GetDiagnosticFieldsFromDiagnosticLine(item)
+    WriteDiagnosticFieldsToFile(id, line, message, source_file)
+
+
+def GetDiagnosticFieldsFromDiagnosticLine(item):
+    print item
+    source_file = re.search('\/.*\..', item).group(0)
+    line = re.search(':\d*:', item).group(0).replace(':', '')
+    id = re.search('\[.*\]', item).group(0).replace('[', '').replace(']', '') + '-clang-compiler'
+    message = re.search('warning: (.*)\[', item).group(0).replace('[', '').replace('warning: ', '')
+    return id, line, message, source_file
+
+
+def WriteDiagnosticFieldsToFile(id, line, message, source_file):
+    clang_sonar_report.write(" <error file=\"" + str(source_file) +
+                        "\" line=\"" + str(line) +
+                        "\" id=\"" + str(id) +
+                        "\" msg=\"" + escape(str(message)) + "\"/>\n")
+
+
+def CreateOutputFile():
+    file_to_write = open('clang_compiler_report.xml', 'w')
+    file_to_write.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    file_to_write.write('<results>\n')
+    return file_to_write
+
+
+def ReadCompilerReportFile():
+    file = open('clang_compiler_report_formatted', 'r')
+    messages_xml = file.readlines()
+    return messages_xml
+
+
+def CloseOutputFile():
+    clang_sonar_report.write('</results>\n')
+    clang_sonar_report.close()
+
+
+def WriteSonarRulesToOutputFile():
+    item_list = clang_compiler_report
+    for item in item_list:
+        ConvertDiagnosticLineToSonqarqube(item)
+
+
+if __name__ == '__main__':
+    clang_sonar_report = CreateOutputFile()
+    clang_compiler_report = ReadCompilerReportFile()
+
+    WriteSonarRulesToOutputFile()
+    CloseOutputFile()
